f8882464cb3e7386b9e8e4e1b70239297a5ae096
Alexander van Oene <alexander.h.van_oene@aggiemail.usu.edu>
2018-05-08 09:18:30 -0500

    imported server into dashapp.py

--- dashapp.py
+++ dashapp.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_table_experiments as dt
 
-from app import app
+from app import app, server
 from pages import single_ticker, portfolio_metrics
 
 
